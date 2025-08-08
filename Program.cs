c2b86f8ee7b12a94bcf14b935cd63b57f94ce5bb
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-08 21:50:58 +0530

    Method example

--- Program.cs
+++ Program.cs
@@ -3,26 +3,15 @@
     public static void Main(string[] args)
     {
 
-        //Jagged array
-
-        int[][] array = new int[3][];
-
-        array[0] = new int[] { 1, 2, 3, 4 };
-        array[1] = new int[] { 4, 5, 6, 7 };
-        array[2] = new int[] { 8, 9, 0, 1 };
-
-        for(int i=0; i<array.GetLength(0);i++)
-        {
-            for (int j = 0; j < array[i].Length;j++)
-            {
-                Console.Write(array[i][j] + " ");
-            }
-
-            Console.WriteLine();
-        }
-
-        
+     
+        Program p = new Program();
+        p.Show();
 
         Console.ReadLine();
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Welcome to C# programming");
     }
 }
