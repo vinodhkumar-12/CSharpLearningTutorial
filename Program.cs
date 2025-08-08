aee1c6309cda7aebaea5dab31a648bb0d9bd73f4
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-08 20:13:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        const double pi = 3.14;
+
+        string FName = "Vinodh";
+        string LName = "Kumar";
+
+        Console.WriteLine("Hello World!");
+        Console.ReadLine();
+
+        Console.WriteLine("My name is {0} {1}",FName,LName);
+    }
+}
