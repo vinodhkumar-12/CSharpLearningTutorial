ecaa7b2219b0578c4063572c6a30af482823f7de
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-08 20:28:22 +0530

    for loop example

--- Program.cs
+++ Program.cs
@@ -2,35 +2,13 @@
 {
     public static void Main(string[] args)
     {
-        int week_number = 10;
-
-        switch(week_number)
+        for (int i = 0; i < 10; i++)
         {
-            case 0:
-                Console.WriteLine("Week Day is Sunday");
-                break;
-            case 1:
-                Console.WriteLine("Week Day is Monday");
-                break;
-            case 2:
-                Console.WriteLine("Week Day is Tuesday");
-                break;
-            case 3:
-                Console.WriteLine("Week Day is Wednesday");
-                break;
-            case 4:
-                Console.WriteLine("Week Day is Thursday");
-                break;
-            case 5:
-                Console.WriteLine("Week Day is Friday");
-                break;
-            case 6:
-                Console.WriteLine("Week Day is Saturday");
-                break;
-            default:
-                Console.WriteLine("No Week Day exists for the mentioned week number");
-                break;
+            Console.WriteLine(i);
         }
 
+        Console.WriteLine("Loop terminates");
+
+        Console.ReadLine();
     }
 }
