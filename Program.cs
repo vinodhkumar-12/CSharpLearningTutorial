b464115333e502dd16e292b1111ab966ee18d6c0
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-08 21:20:43 +0530

    array example

--- Program.cs
+++ Program.cs
@@ -3,20 +3,13 @@
     public static void Main(string[] args)
     {
 
-        int num1 = 0;
-        int num2 = 0;
+        int[] array = new int[6];
 
-        int result = 0;
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
 
-        Console.WriteLine("Enter the number 1");
-        num1 = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Enter the number 2");
-        num2 = Convert.ToInt32(Console.ReadLine());
-
-        result = num1 + num2;
-
-        Console.WriteLine("Sum of two number is {0}", result);
+        Console.WriteLine(array[0]);
 
         Console.ReadLine();
     }
