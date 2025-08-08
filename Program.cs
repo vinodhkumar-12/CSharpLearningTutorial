9bdcef08a936deaed423631b68bceda01bacc5a1
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-08 22:28:55 +0530

    paramter example

--- Program.cs
+++ Program.cs
@@ -3,12 +3,24 @@
     public static void Main(string[] args)
     {
 
-        Program.Show();
-       
+        int num1 = 0;
+        Console.WriteLine("Enter the number 1");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        int num2 = 0;
+        Console.WriteLine("Enter the number 2");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Program p = new Program();
+        
+        int result = p.AddNumbers(num1, num2);
+
+        Console.WriteLine("Sum of Two Number is : {0}", result);
+
     }
 
-    public static void Show()
+    public int AddNumbers(int num1, int num2)
     {
-        Console.WriteLine("Welcome to C# programming");
+        return num1 + num2;
     }
 }
