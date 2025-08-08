d7a397528efa503c247d0cde5e7d76fd421e13bd
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-08 21:39:58 +0530

    Multi-dimensional rectangular array foreach example

--- Program.cs
+++ Program.cs
@@ -15,14 +15,19 @@
         //Console.WriteLine(array.GetLength(0));
         //Console.WriteLine(array.GetLength(1));
 
-        for(int i= 0; i < array.GetLength(0); i++)
-        {
-            for(int j=0; j< array.GetLength(1);j++)
-            {
-                Console.Write(array[i, j] + " ");
-            }
+        //for(int i= 0; i < array.GetLength(0); i++)
+        //{
+        //    for(int j=0; j< array.GetLength(1);j++)
+        //    {
+        //        Console.Write(array[i, j] + " ");
+        //    }
 
-            Console.WriteLine();
+        //    Console.WriteLine();
+        //}
+
+        foreach (int i in array)
+        {
+            Console.WriteLine(i);
         }
 
         Console.ReadLine();
