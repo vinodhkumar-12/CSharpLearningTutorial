2bde695115f08cf80812066504ec470d04847c31
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-08 21:38:45 +0530

    Multi-dimensional rectangualr array example

--- Program.cs
+++ Program.cs
@@ -3,21 +3,26 @@
     public static void Main(string[] args)
     {
 
-        string[] names = new string[4];
+        //Multi - dimensional array
 
-        names[0] = "vinodh";
-        names[1] = "kumar";
-        names[2] = "vijay";
-        names[3] = "balakrishnan";
-
-        //for (int i = 0; i < name.Length; i++)
-        //{
-        //    Console.WriteLine(name[i]);
-        //}
-
-        foreach(var name in names)
+        int[,] array = new int[3, 4]
         {
-            Console.WriteLine(name);
+            {11,22,33,44 },
+            {55,66,77,88 },
+            {99,11,22,33 }
+        };
+
+        //Console.WriteLine(array.GetLength(0));
+        //Console.WriteLine(array.GetLength(1));
+
+        for(int i= 0; i < array.GetLength(0); i++)
+        {
+            for(int j=0; j< array.GetLength(1);j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+
+            Console.WriteLine();
         }
 
         Console.ReadLine();
