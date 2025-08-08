1487c13bc1ee78c7bdfdf18faeb545e10f08fdb6
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-08 21:16:20 +0530

    arithmetic operator example

--- Program.cs
+++ Program.cs
@@ -3,28 +3,20 @@
     public static void Main(string[] args)
     {
 
-        //Jump Break statment
+        int num1 = 0;
+        int num2 = 0;
 
-        int a = 10;
+        int result = 0;
 
+        Console.WriteLine("Enter the number 1");
+        num1 = Convert.ToInt32(Console.ReadLine());
 
-        if (a == 10)
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                if (i == 5)
-                {
-                    goto stop;
-                }
-                Console.WriteLine(i);
-            }
-        }
+        Console.WriteLine("Enter the number 2");
+        num2 = Convert.ToInt32(Console.ReadLine());
 
+        result = num1 + num2;
 
-        Console.WriteLine("Loop terminates");
-
-    stop:
-        Console.WriteLine("Terminates to here");
+        Console.WriteLine("Sum of two number is {0}", result);
 
         Console.ReadLine();
     }
