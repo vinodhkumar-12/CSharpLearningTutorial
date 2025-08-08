fb7d43b4ff682bf1d37878a4aeec4a84fe954668
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-08 20:37:36 +0530

    break statment example

--- Program.cs
+++ Program.cs
@@ -2,13 +2,24 @@
 {
     public static void Main(string[] args)
     {
-        int i = 0;
 
-        do
+        //Jump Break statment
+
+        int a = 10;
+
+
+        if (a == 10)
         {
-            Console.WriteLine(i);
-            i++;
-        } while (i < 10);
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+        }
+
 
         Console.WriteLine("Loop terminates");
 
