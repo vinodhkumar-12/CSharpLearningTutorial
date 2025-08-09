3121af945f5250a4eb034246ffee0c78a4d5b673
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-09 18:54:27 +0530

    stack example

--- Program.cs
+++ Program.cs
@@ -4,31 +4,30 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Hashtable hashtable = new Hashtable();
-        hashtable.Add("Id", 1);
-        hashtable.Add("FirstName", "Vinodh");
-        hashtable.Add("LastName", "Kumar");
-        hashtable.Add("Age", 28);
-        // hashtable.Add("Age", 28); same key should not be mentioned more than one in hash table
+        Stack stack = new Stack();
 
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        stack.Push(4);
 
-        hashtable.Remove("Age");
-
-        bool isLastNameExists = hashtable.Contains("LastName");
-
-        foreach (string key in hashtable.Keys)
+        foreach(var item in stack)
         {
-            Console.WriteLine(key + " " + hashtable[key]);
+            Console.WriteLine(item);
         }
 
 
-        Console.WriteLine(isLastNameExists);
-        Console.WriteLine(hashtable.GetHashCode());
+        stack.Pop();
 
+        Console.WriteLine("After Pop done");
 
+        foreach(var item in stack)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("Peek Value is : ");
+
+        Console.WriteLine(stack.Peek());
     }
 }
-
-
-
-
