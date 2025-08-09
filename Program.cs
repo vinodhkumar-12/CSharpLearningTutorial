be679687e85a62a5b07ed5805d88addbc0a8b80e
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-09 12:50:24 +0530

    Inheritance example

--- Program.cs
+++ Program.cs
@@ -1,38 +1,26 @@
-﻿public class Program
+﻿public class DerivedClass : BaseClass
 {
 
     public static void Main(string[] args)
     {
 
-        Person obj_first_name = new Person();
+        DerivedClass derivedClass = new DerivedClass();
 
-        obj_first_name.Name = "Vinodh";
+        derivedClass.show();
+        derivedClass.Name = "Foo";
 
-
-        Person obj_last_name = new Person();
-
-        obj_last_name.Name = "Kumar";
-
-        Person obj_full_name = new Person();
-
-        obj_full_name = obj_first_name + obj_last_name;
-
-        Console.WriteLine("Full name is {0}", obj_full_name.Name);
+        Console.WriteLine(derivedClass.Name);
     }
 
 }
 
-public class Person
+public class BaseClass 
 {
     public string Name { get; set; }
-
-    public static Person operator +(Person obj_first_name, Person obj_last_name)
+   
+    public void show()
     {
-        Person obj_full_name = new Person();
-
-        obj_full_name.Name = obj_first_name.Name + " " + obj_last_name.Name;
-
-        return obj_full_name;
+        Console.WriteLine("Hello World");
     }
 }
 
