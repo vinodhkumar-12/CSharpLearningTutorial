04f857036ffe48f3c8928a6eb0f22bd0a2fa225e
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-09 19:48:07 +0530

    List<T> Add, AddRange, Insert, Insertrange example

--- Program.cs
+++ Program.cs
@@ -2,21 +2,52 @@
 
 public class Program
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        ArrayList list = new ArrayList();
-        int prevCapacity = list.Capacity;
 
-        Console.WriteLine($"Initially Capacity = {prevCapacity}");
 
-        for (int i = 0; i < 20; i++)
+        Employee hari_employee = new Employee() { Age = 28, FirstName = "Harish", LastName = "Laskhmanan" };
+
+
+        List<Employee> employees = new List<Employee>()
         {
-            list.Add(i);
-            if (list.Capacity != prevCapacity)
-            {
-                prevCapacity = list.Capacity;
-                Console.WriteLine($"After adding {i + 1} elements Capacity = {list.Capacity}");
-            }
+            new Employee() { FirstName = "Vinodh", LastName = "Kumar", Age = 28 },
+            new Employee() {  FirstName = "Vijay", LastName = "Kumar", Age = 26 }
+        };
+
+        foreach (Employee employee in employees)
+        {
+            Console.WriteLine(employee.FirstName + " " + employee.LastName);
         }
+
+        Console.WriteLine("Capacity is : " + employees.Capacity);
+        Console.WriteLine("Count is : " + employees.Count);
+
+
+        List<Employee> newemployees = new List<Employee>()
+        {
+            new Employee() { FirstName = "Vasanth", LastName = "Kumar", Age = 26 },
+            new Employee() {FirstName = "Hari", LastName = "Vishal", Age = 26 },
+            new Employee() { FirstName = "Soumya", LastName = "Prakash", Age = 26 }
+        };
+
+        Console.WriteLine("After new employees Added");
+
+        employees.InsertRange(0, newemployees);
+
+
+        employees.Insert(2, hari_employee);
+
+        foreach (Employee employee in employees)
+        {
+            Console.WriteLine(employee.FirstName + " " + employee.LastName);
+        }
+
+    }
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
     }
 }
