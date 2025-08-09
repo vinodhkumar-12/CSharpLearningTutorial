f7a361c346fd606f1ab34d72adcb3c81f5f2c509
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-09 19:57:29 +0530

    List<T> Remove, RemoveAt. RemoveRange, RemoveAll Example

--- Program.cs
+++ Program.cs
@@ -5,44 +5,29 @@ public class Program
     public static void Main(string[] args)
     {
 
-
-        Employee hari_employee = new Employee() { Age = 28, FirstName = "Harish", LastName = "Laskhmanan" };
-
-
         List<Employee> employees = new List<Employee>()
         {
             new Employee() { FirstName = "Vinodh", LastName = "Kumar", Age = 28 },
-            new Employee() {  FirstName = "Vijay", LastName = "Kumar", Age = 26 }
+            new Employee() {  FirstName = "Vijay", LastName = "Kumar", Age = 26 },
+             new Employee() {  FirstName = "Harish", LastName = "Lakshmanan", Age = 28 },
+              new Employee() {  FirstName = "Vasanth", LastName = "Kumar", Age = 27 },
+               new Employee() {  FirstName = "Naresh", LastName = "Pandi", Age = 28 }
         };
+
+        //Employee hari_employee = employees.Where(x => x.FirstName == "Harish").FirstOrDefault();
+
+        //employees.Remove(hari_employee);
+
+        //employees.RemoveAt(1);
+
+        //employees.RemoveRange(1, 2);
+
+        employees.RemoveAll(x => x.Age == 28);
 
         foreach (Employee employee in employees)
         {
             Console.WriteLine(employee.FirstName + " " + employee.LastName);
         }
-
-        Console.WriteLine("Capacity is : " + employees.Capacity);
-        Console.WriteLine("Count is : " + employees.Count);
-
-
-        List<Employee> newemployees = new List<Employee>()
-        {
-            new Employee() { FirstName = "Vasanth", LastName = "Kumar", Age = 26 },
-            new Employee() {FirstName = "Hari", LastName = "Vishal", Age = 26 },
-            new Employee() { FirstName = "Soumya", LastName = "Prakash", Age = 26 }
-        };
-
-        Console.WriteLine("After new employees Added");
-
-        employees.InsertRange(0, newemployees);
-
-
-        employees.Insert(2, hari_employee);
-
-        foreach (Employee employee in employees)
-        {
-            Console.WriteLine(employee.FirstName + " " + employee.LastName);
-        }
-
     }
     public class Employee
     {
