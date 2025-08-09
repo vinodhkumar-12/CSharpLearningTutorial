a77767a733cfa24ae876d96002f922c18ca75942
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-09 14:50:16 +0530

    sealed method example

--- Program.cs
+++ Program.cs
@@ -1,24 +1,19 @@
 ﻿public class DerivedClass : BaseClass
 {
 
-    public static void Main(string[] args)
-    {
-
-        //DerivedClass derivedClass = new DerivedClass();
-        //derivedClass.show();
-
-        //BaseClass baseClass = new BaseClass();
-        //baseClass.show();
-
-        BaseClass baseClass1 = new DerivedClass();
-        baseClass1.show();
-    }
-
-    public override void show()
+    public sealed override void show()
     {
         Console.WriteLine("Hello World from Derived Class itself!!!");
     }
 
+}
+
+public class SubDerivedClass : DerivedClass
+{
+    //public override void show()
+    //{
+    //    Console.WriteLine("Hello World from Sub Derived Class itself!!!");
+    //}
 }
 
 public class BaseClass
@@ -27,6 +22,16 @@ public class BaseClass
     public virtual void show()
     {
         Console.WriteLine("Hello World from Base Class");
+    }
+}
+
+
+public class Progran
+{
+    public static void Main(string[] args)
+    {
+        BaseClass baseClass = new SubDerivedClass();
+        baseClass.show();
     }
 }
 
