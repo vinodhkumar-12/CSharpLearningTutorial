e5c5490973c3e18d261a30e709655e807edbdaf2
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-10 23:18:40 +0530

    If thread/task not provided execution of the program in reality

--- Program.cs
+++ Program.cs
@@ -7,34 +7,34 @@ using System.Text.Json;
 
 public class Program
 {
-    private static object _lock = new object();
-    //private static Semaphore _semaphore = new Semaphore(1, 1);
-
-    private static Semaphore _semaphore = new Semaphore(2, 2);
     public static void Main(string[] args)
     {
-
         Program p = new Program();
+        Console.WriteLine("Main Thread starting...");
 
-        for (int i = 0; i < 5; i++)
-        {
-            Thread t = new Thread(p.Write);
+        p.GetDatabaseDetail();
+        p.CallRespectiveDatabase();
 
-            t.Start();
-        }
+        Console.WriteLine("Main Thread completed...");
 
-        //Thread.Sleep(2000);
-        //_mre.Set();
     }
 
-
-    public void Write()
+    public void GetDatabaseDetail()
     {
-        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is Waiting...");
-        _semaphore.WaitOne();
-        Thread.Sleep(3000);
-        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Writing is Completed...");
-        _semaphore.Release();
+        Console.WriteLine($"Function Name GetDatabaseDetail from {Thread.CurrentThread.ManagedThreadId} is calling...");
+
+        Thread.Sleep(2000);
+
+        Console.WriteLine($"Function Name GetDatabaseDetail from {Thread.CurrentThread.ManagedThreadId} is completed...");
+    }
+
+    public void CallRespectiveDatabase()
+    {
+        Console.WriteLine($"Function Name CallRespectiveDatabase from {Thread.CurrentThread.ManagedThreadId} is calling...");
+
+        Thread.Sleep(2000);
+
+        Console.WriteLine($"Function Name CallRespectiveDatabase from {Thread.CurrentThread.ManagedThreadId} is completed...");
     }
 
 }
