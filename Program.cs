93af60553f8493bdf0c15098261255990eb2d182
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-10 10:57:27 +0530

    is keyword example

--- Program.cs
+++ Program.cs
@@ -6,21 +6,10 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        string filepath = @"D:\MyFile.txt";
+        string name = "vinodh";
+        bool check = name is string;
 
-        using (FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read))
-        {
-            using (StreamReader reader = new StreamReader(fs))
-            {
-                string data = reader.ReadToEnd();
-
-                Console.WriteLine(data);
-
-            }
-
-        }
-
-        Console.WriteLine("Stream Read append done succesfully");
+        Console.WriteLine(check);
 
     }
 }
