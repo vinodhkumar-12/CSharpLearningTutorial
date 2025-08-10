6636029d05b2411b03e0bbf02c64ce359898c9aa
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-10 21:00:01 +0530

    Thread Synchornization lock example

--- Program.cs
+++ Program.cs
@@ -7,33 +7,43 @@ using System.Text.Json;
 
 public class Program
 {
+    private static object _lock = new object();
     public static void Main(string[] args)
     {
-        string[] members = Enum.GetNames(typeof(Days));
 
-        foreach (string member in members)
+        Program p = new Program();
+
+        //new Thread(p.Function).Start();
+
+        //Thread t1 = new Thread(p.Function);
+        //Thread t2 = new Thread(p.Function);
+        //Thread t3 = new Thread(p.Function);
+        //Thread t4 = new Thread(p.Function);
+        //Thread t5 = new Thread(p.Function);
+
+        //t1.Start();
+        //t2.Start();
+        //t3.Start();
+        //t4.Start();
+        //t5.Start();
+
+        for (int i = 0; i < 5; i++)
         {
-            Console.WriteLine(member);
+            Thread t = new Thread(p.Function);
+
+            t.Start();
         }
-
-        int[] values = (int[])Enum.GetValues(typeof(Days));
-
-        foreach (int value in values)
-        {
-            Console.WriteLine(value);
-        }
-
     }
 
-}
 
-public enum Days
-{
-    Sunday,
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday
+    public void Function()
+    {
+        lock (_lock)
+        {
+            Console.WriteLine("Thread Id is : {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Thread Id is {0} is Sleeping", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(5000);
+            Console.WriteLine("Thread Id is {0} is Completed", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
 }
