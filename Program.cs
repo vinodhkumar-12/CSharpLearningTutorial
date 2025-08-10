ffce332fecbe120c91826a189f3ccb4b8ef0d453
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-10 11:22:24 +0530

    serialization example

--- Program.cs
+++ Program.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Text;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        string name = "vinodh";
+        string FilePath = @"D:\MyFile.txt";
 
-        string conversionname = name as string;
+        Employee employee = new Employee() { Id = 1, Name = "Vinodh" };
 
-        Console.WriteLine(conversionname);
+        string jsonString = JsonSerializer.Serialize(employee);
+
+        File.WriteAllText(FilePath, jsonString);
 
     }
+}
+
+
+[Serializable]
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
 }
