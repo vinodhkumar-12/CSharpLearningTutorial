89acb78b9e3f2e306dba499b600ead4399aa52f2
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-10 22:37:01 +0530

    AutoResetEvent Thread Synchornoization Example

--- Program.cs
+++ Program.cs
@@ -8,38 +8,31 @@ using System.Text.Json;
 public class Program
 {
     private static object _lock = new object();
-    private static ManualResetEvent _mre = new ManualResetEvent(false);
+    private static AutoResetEvent _mre = new AutoResetEvent(true);
     public static void Main(string[] args)
     {
 
         Program p = new Program();
 
-        new Thread(p.Write).Start();
-
         for (int i = 0; i < 5; i++)
         {
-            Thread t = new Thread(p.Read);
+            Thread t = new Thread(p.Write);
 
             t.Start();
         }
+
+        //Thread.Sleep(2000);
+        //_mre.Set();
     }
 
 
     public void Write()
     {
-        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is Writing...");
-        _mre.Reset();
-        Thread.Sleep(3000);
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is Waiting...");
+        _mre.WaitOne();
+        Thread.Sleep(8000);
         Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Writing is Completed...");
         _mre.Set();
-
     }
 
-    public void Read()
-    {
-        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is Reading...");
-        //Thread.Sleep(5000);
-        _mre.WaitOne();
-        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Reading is Completed...");
-    }
 }
